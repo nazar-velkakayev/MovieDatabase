4f46996ea860c35a69a21c367c6f4a2c941af797
Nazar <nazarwelkakayew41@gmail.com>
2022-11-12 15:39:52 +0500

    Initial Commit

---
+++ MovieDatabase/MovieDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseApp.swift
+//  MovieDatabase
+//
+//  Created by Belli's MacBook on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
